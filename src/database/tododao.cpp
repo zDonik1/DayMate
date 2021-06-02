1a88f26cc9856d3f0836271cac2993589cba3352
zDonik <tokhirovdoniyor@gmail.com>
2021-06-02 22:33:11 +0500

    feat: added functionality for CRUD operations on todos

--- src/database/tododao.cpp
+++ src/database/tododao.cpp
@@ -6,6 +6,55 @@
 
 #include "tododao.h"
 
+#include <QSqlDatabase>
+#include <QStringList>
+#include <QVariant>
+
+static constexpr auto TABLE = "todo";
+
 TodoDao::TodoDao()
 {
+    auto database = QSqlDatabase::database();
+    if (!database.tables().contains(TABLE)) {
+        executeQuery(QString("create table %1 ("
+                             "uuid text primary key," // id is a uuid
+                             "todo text"
+                             ")")
+                     .arg(TABLE));
+    }
+}
+
+bool TodoDao::add(const Todo &todo) const
+{
+    return executeQuery(QString("insert into %1 (uuid, todo) values('%2', '%3')")
+                        .arg(TABLE, QUuid::createUuid().toString(QUuid::WithoutBraces),
+                             todo.text)).first;
+}
+
+bool TodoDao::edit(const Todo &todo) const
+{
+    return executeQuery(QString("update %1 set todo='%2' where uuid='%3'")
+                        .arg(TABLE, todo.text,
+                             todo.uuid.toString(QUuid::WithoutBraces))).first;
+}
+
+bool TodoDao::remove(const QUuid &uuid) const
+{
+    return executeQuery(QString("delete from %1 where uuid='%2'")
+                        .arg(TABLE, uuid.toString(QUuid::WithoutBraces))).first;
+}
+
+QList<Todo> TodoDao::get() const
+{
+    auto [success, query] = executeQuery(QString("select * from %1").arg(TABLE));
+    if (!success) return {};
+
+    QList<Todo> todos;
+    while (query.next()) {
+        Todo todo;
+        todo.uuid = query.value(0).toString();
+        todo.text = query.value(1).toString();
+        todos.push_back(std::move(todo));
+    }
+    return todos;
 }
--- src/database/tododao.h
+++ src/database/tododao.h
@@ -6,11 +6,23 @@
 
 #pragma once
 
+#include <QUuid>
+
 #include "abstractdao.h"
+
+struct Todo {
+    QUuid uuid;
+    QString text;
+};
 
 class TodoDao : public AbstractDao
 {
 public:
     TodoDao();
+
+    bool add(const Todo &todo) const;
+    bool edit(const Todo &todo) const;
+    bool remove(const QUuid &uuid) const;
+    QList<Todo> get() const;
 };
 
